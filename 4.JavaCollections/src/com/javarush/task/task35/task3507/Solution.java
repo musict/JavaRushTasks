66b0572c1b9933c39f54f49bfac8bcd9b9629ce8
musicy <musicy@yandex.ru>
2018-05-05 16:30:51 +0300

    Task 3507 has done

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,5 +1,10 @@
 package com.javarush.task.task35.task3507;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
 import java.util.Set;
 
 /* 
@@ -12,7 +17,53 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        //C:\Users\Python\Downloads\JavaRushTasks\4.JavaCollections\src\com\javarush\task\task35\task3507\data
-        return null;
+
+        Set<Animal> animals = new HashSet<>();
+        if (!pathToAnimals.endsWith("\\") && !pathToAnimals.endsWith("/")) pathToAnimals += "/";
+        final File f = new File(pathToAnimals);
+        File[] files = f.listFiles((dir, name) -> name.toLowerCase().endsWith(".class"));
+        for (final File file : files) {
+            ClassLoader classLoader = new ClassLoader() {
+                @Override
+                public Class<?> findClass(String name) throws ClassNotFoundException {
+                    try {
+                        byte[] buffer = java.nio.file.Files.readAllBytes(file.toPath());
+                        return defineClass(null, buffer, 0, buffer.length);
+                    }
+                    catch (IOException e) {
+                        return super.findClass(name);
+                    }
+                }
+            };
+            String shortName = file.getName().substring(0, file.getName().length() - 6);
+            try {
+                Class clazz = classLoader.loadClass(shortName);
+                boolean hasInterface = false;
+                Class[] interfaces = clazz.getInterfaces();
+                for (Class c : interfaces) {
+                    if (c.equals(Animal.class)) {
+                        hasInterface = true;
+                        break;
+                    }
+                }
+                if (!hasInterface) continue;
+                boolean hasConstructor = false;
+                Constructor[] constructors = clazz.getConstructors();
+                for (Constructor c : constructors) {
+                    c.setAccessible(true);
+                    if (!Modifier.isAbstract(c.getModifiers()) && Modifier.isPublic(c.getModifiers())) {
+                        hasConstructor = true;
+                        break;
+                    }
+                    else if (Modifier.isPrivate(c.getModifiers())) hasConstructor = false;
+                }
+                if (!hasConstructor) continue;
+                animals.add((Animal) clazz.newInstance());
+            }
+            catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return animals;
     }
 }
